f26ab5b95d6c9bc533680047f005d4ff95f7264a
giginet <giginet.net@gmail.com>
2013-03-05 23:29:40 +0900

    QWUserを実装

--- Qwi/Model/QWUser.h
+++ Qwi/Model/QWUser.h
@@ -15,16 +15,18 @@
 @interface QWUser : NSObject {
 }
 
-@property(readonly) NSString* username;
-@property(readonly) NSString* displayName;
-@property(readonly) NSArray* following;
-@property(readonly) NSArray* follower;
-@property(readonly) NSArray* tweets;
-@property(readonly) NSArray* favorites;
-@property(readonly) UIImage* icon;
+@property(readonly) NSString* name;
+@property(readonly) NSString* screenName;
+@property(readonly) NSString* description;
+@property(readonly) NSUInteger friendsCount;
+@property(readonly) NSUInteger favoritesCount;
+@property(readonly) NSUInteger statusesCount;
+@property(readonly) NSUInteger followersCount;
+@property(readonly) NSUInteger listedCount;
 @property(readonly) NSString* location;
-@property(readonly) NSURL* url;
-@property(readonly) NSString* bio;
+@property(readonly) NSURL* URL;
+@property(readonly) NSURL* profileImageURL;
+@property(readonly) UIImage* profileImage;
 
 - (id)initWithJSON:(NSString*)jsonString;
 
--- Qwi/Model/QWUser.m
+++ Qwi/Model/QWUser.m
@@ -5,9 +5,43 @@
 //  Created by giginet on 3/5/13.
 //  Copyright (c) 2013 giginet. All rights reserved.
 //
+#include "CJSONDeserializer.h"
 
 #import "QWUser.h"
 
+@interface QWUser(Private)
+- (void)parseWithJSON:(NSString*)jsonString;
+@end
+
 @implementation QWUser
+
+- (id)initWithJSON:(NSString *)jsonString {
+  self = [super init];
+  if (self) {
+    [self parseWithJSON:jsonString];
+  }
+  return self;
+}
+
+- (void)parseWithJSON:(NSString *)jsonString {
+  NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+  NSError *error = nil;
+  NSDictionary *dictionary = [[CJSONDeserializer deserializer] deserializeAsDictionary:jsonData error:&error];
+  self.name = [[dictionary objectForKey:@"name"] stringValue];
+  self.screenName = [[dictionary objectForKey:@"screen_name"] stringValue];
+  self.description = [[dictionary objectForKey:@"description"] stringValue];
+  self.friendsCount = [[dictionary objectForKey:@"friend_count"] integerValue];
+  self.favoritesCount = [[dictionary objectForKey:@"favorites_count"] integerValue];
+  self.statusesCount = [[dictionary objectForKey:@"statuses_count"] integerValue];
+  self.followersCount = [[dictionary objectForKey:@"followers_count"] integerValue];
+  self.listedCount = [[dictionary objectForKey:@"listed_count"] integerValue];
+  if ([dictionary objectForKey:@"url"]) {
+    NSString* urlString = [[dictionary objectForKey:@"url"] stringValue];
+    self.URL = [NSURL URLWithString:urlString];
+  }
+  
+  self.profileImageURL = [NSURL URLWithString:[[dictionary objectForKey:@"profile_image_url"] stringValue]];
+  self.location = [[dictionary objectForKey:@"location"] stringValue];
+}
 
 @end
